1324f0c300d16753cf7df3bd77550ea895e33772
Ayobami <tnenas07@gmail.com>
2023-06-22 00:44:21 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+        int i;
+
+        i = 0; /* Change the value here to test different cases */
+
+        positive_or_negative(i);
+
+        return (0);
+}
