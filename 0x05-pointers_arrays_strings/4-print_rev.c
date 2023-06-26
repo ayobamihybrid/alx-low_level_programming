d5d83a14e296480cc3ad93801afed844a2ff77e2
Ayobami <tnenas07@gmail.com>
2023-06-26 01:59:01 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: Pointer to the string to be printed.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Print the string in reverse order */
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
