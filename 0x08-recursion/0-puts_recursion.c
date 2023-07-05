c9ce970682eaeddc668ab54253bbd13306d1ec1a
Ayobami <tnenas07@gmail.com>
2023-07-05 08:27:52 +0100

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,9 +4,9 @@
  * @s: input
  * Return: Always 0 (Success);
  */
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s) 
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
