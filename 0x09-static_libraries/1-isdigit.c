b8e660bc586c6768dd31eff3b2caee294ed9873d
Ayobami <tnenas07@gmail.com>
2023-07-07 13:34:17 +0100

    update

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,32 +1,12 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most n bytes from src
- * @dest: Pointer to the destination string
- * @src: Pointer to the source string
- * @n: Maximum number of bytes to be appended from src
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The integer to be checked
  *
- * Return: Pointer to the resulting string
+ * Return: 1 if c is a digit, 0 otherwise
  */
-char *_strncat(char *dest, char *src, int n)
+int _isdigit(int c)
 {
-	int i, j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
+	return (c >= '0' && c <= '9');
 }
