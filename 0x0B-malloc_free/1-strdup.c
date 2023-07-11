676c3ab5e16becbab58706667b947268e6f850c2
Ayobami <tnenas07@gmail.com>
2023-07-11 07:42:41 +0100

    create_array & 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string to a new memory space location
+ * @str: string to be duplicated
+ *
+ * Return: Pointer to the duplicated string, or NULL if failed
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int i, length = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+
+	duplicate = malloc(sizeof(char) * (length + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		duplicate[i] = str[i];
+
+	return (duplicate);
+}
