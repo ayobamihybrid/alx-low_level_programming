eb9717ee5d39165841df1203c7313b037f9be019
Ayobami <tnenas07@gmail.com>
2023-07-28 08:20:36 +0100

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,24 +2,27 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
+ * print_list - prints all elements of a linked list
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-    size_t s = 0;
+	size_t s = 0; // Initialize the counter for the number of nodes
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        s++;
-    }
+	// Loop through the linked list until h becomes NULL (end of the list)
+	while (h)
+	{
+		// Check if the string is NULL (empty)
+		if (!h->str)
+			printf("[0] (nil)\n"); // Print "nil" for an empty string
+		else
+			printf("[%u] %s\n", h->len, h->str); // Print the string with its length
 
-    return (s);
+		h = h->next; // Move to the next node in the linked list
+		s++; // Increment the counter for the number of nodes printed
+	}
+
+	return (s); // Return the total number of nodes printed
 }
