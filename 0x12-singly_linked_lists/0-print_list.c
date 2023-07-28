067bee7693aba32afcf895e724cc126f2a721365
Ayobami <tnenas07@gmail.com>
2023-07-28 09:11:32 +0100

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,6 @@
 size_t print_list(const list_t *h)
 {
 size_t s = 0; /* Initialize the counter for the number of nodes */
-/* Loop through the linked list until h becomes NULL (end of the list) */
 while (h)
 {
 /* Check if the string is NULL (empty) */
@@ -19,7 +18,7 @@ printf("[0] (nil)\n"); /* Print "nil" for an empty string */
 else
 printf("[%u] %s\n", h->len, h->str); /* Print the string with its length */
 h = h->next; /* Move to the next node in the linked list */
-s++; /* Increment the counter for the number of nodes printed */
+s++;
 }
 return (s); /* Return the total number of nodes printed */
 }
